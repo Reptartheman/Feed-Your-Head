55e64f38d79a4e8f71a1ef66fc2a96675aff2a52
Jeff Moro <jeffmoromusic@gmail.com>
2024-04-11 20:25:23 -0400

    changed the UI to spinning record and added side menu

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const playlistSongs = document.getElementById("playlist-songs");
-const playButton = document.getElementById("play");
-const pauseButton = document.getElementById("pause");
-const nextButton = document.getElementById("next");
-const previousButton = document.getElementById("previous");
-const shuffleButton = document.getElementById("shuffle");
+const audio = new Audio();
+const controls = document.querySelector('.controls');
+const playButton = document.getElementById('playButton');
+const vinyl = document.getElementsByClassName('vinyl');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
-const currTime = document.querySelector('#currTime');
-const durTime = document.querySelector('#durTime');
+let isPlaying = false;
+let navbarNav = document.querySelector('.navbar-nav')
+let navbarList = document.getElementById('navbarList');
+let windowWidth = window.innerWidth;
+let hamburgerMenu = document.querySelector('.hamburger');
+const navButton = document.querySelector('button[aria-expanded]');
+const nav = document.getElementById("topNav");
 const allSongs = [
   {
     id: 0,
@@ -40,71 +45,93 @@ const allSongs = [
 
 ];
 
-const audio = new Audio();
+
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
 
-const playSong = (id) => {
+
+function playSong(id) {
   const song = userData?.songs.find((song) => song.id === id);
   audio.src = song.src;
   audio.title = song.title;
-
+  
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
     audio.currentTime = 0;
   } else {
     audio.currentTime = userData.songCurrentTime;
   }
+
   userData.currentSong = song;
-  playButton.classList.add("playing");
 
-  highlightCurrentSong();
-  setPlayerDisplay();
-  setPlayButtonAccessibleText();
-  audio.play();
-};
-
-const pauseSong = () => {
-  userData.songCurrentTime = audio.currentTime;
+  document.querySelector('.vinyl-container').classList.add('play');
+  playButton.querySelector('i').classList.remove('icon-play');
+  playButton.querySelector('i').classList.add('icon-pause');
+  for (let i = 0; i < vinyl.length; i++) {
+    vinyl[i].style.animationPlayState = 'running';
+  }
+  isPlaying = true;
   
-  playButton.classList.remove("playing");
-  audio.pause();
-};
+  setPlayerDisplay();
+  audio.play();
+  
+}
 
-const playNextSong = () => {
+
+function setPlayerDisplay() {
+  const trackTitle = document.getElementById('title');
+  const trackArtist = document.getElementById('artist');
+
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  trackTitle.textContent = currentTitle ? currentTitle : "";
+  trackArtist.textContent = currentArtist ? currentArtist : "";
+
+}
+
+
+function pauseSong() {
+  userData.songCurrentTime = audio.currentTime;
+  document.querySelector('.vinyl-container').classList.remove('play');
+
+  playButton.querySelector('i').classList.add('icon-play');
+  playButton.querySelector('i').classList.remove('icon-pause');
+  for (let i = 0; i < vinyl.length; i++) {
+    vinyl[i].style.animationPlayState = 'paused';
+  }
+
+  isPlaying = false;
+  audio.pause();
+}
+
+function playNextSong() {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
     const currentSongIndex = getCurrentSongIndex();
     const nextSong = userData?.songs[currentSongIndex + 1];
-
     playSong(nextSong.id);
   }
-};
 
-const playPreviousSong = () =>{
-   if (userData?.currentSong === null) return;
-   else {
-    const currentSongIndex = getCurrentSongIndex();
-    const previousSong = userData?.songs[currentSongIndex - 1];
+  
+}
 
-    playSong(previousSong.id);
-   }
-};
+function playPreviousSong() {
+  if (userData?.currentSong === null) return;
+  else {
+   const currentSongIndex = getCurrentSongIndex();
+   const previousSong = userData?.songs[currentSongIndex - 1];
 
-const shuffle = () => {
-  userData?.songs.sort(() => Math.random() - 0.5);
-  userData.currentSong = null;
-  userData.songCurrentTime = 0;
+   playSong(previousSong.id);
+  }
+}
 
-  //renderSongs(userData?.songs);
-  pauseSong();
-  setPlayerDisplay();
-  setPlayButtonAccessibleText();
-};
 
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData.currentSong);
 
 
 function updateProgress(e) {
@@ -114,101 +141,49 @@ function updateProgress(e) {
 }
 
 function setProgress(e) {
-  const width = this.clientWidth
+  const width = this.clientWidth 
   const clickX = e.offsetX;
   const duration = audio.duration
-
-  audio.currentTime = (clickX / width) * duration;
+  
+  audio.currentTime = (clickX / width) * duration;  
 };
 
-const setPlayerDisplay = () => {
-  const playingSong = document.getElementById("player-song-title");
-  const songArtist = document.getElementById("player-song-artist");
-  const currentTitle = userData?.currentSong?.title;
-  const currentArtist = userData?.currentSong?.artist;
 
-  playingSong.textContent = currentTitle ? currentTitle : "";
-  songArtist.textContent = currentArtist ? currentArtist : "";
-};
-
-const highlightCurrentSong = () => {
-  const playlistSongElements = document.querySelectorAll(".playlist-song");
-  const songToHighlight = document.getElementById(
-    `song-${userData?.currentSong?.id}`
-  );
-
-  playlistSongElements.forEach((songEl) => {
-    songEl.removeAttribute("aria-current");
-  });
-
-  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
-};
-
-/* const renderSongs = (array) => {
-  const songsHTML = array
-    .map((song)=> {
-      return `
-      <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info" onclick="playSong(${song.id})">
-          <span class="playlist-song-title">${song.title}</span>
-          <span class="playlist-song-artist">${song.artist}</span>
-          <span class="playlist-song-duration">${song.duration}</span>
-      </button>
-      </li>
-      `;
-    })
-    .join("");
-
-  playlistSongs.innerHTML = songsHTML;
-}; */
-
-const setPlayButtonAccessibleText = () => {
-  const song = userData?.currentSong || userData?.songs[0];
-
-  playButton.setAttribute(
-    "aria-label",
-    song?.title ? `Play ${song.title}` : "Play"
-  );
-};
-
-const getCurrentSongIndex = () => userData?.songs.indexOf(userData.currentSong);
-
-playButton.addEventListener("click", () => {
-    if (userData?.currentSong === null) {
-    playSong(userData?.songs[0].id);
-  } else {
-    playSong(userData?.currentSong.id);
-  }
+playButton.addEventListener('click', () => {
+  if(isPlaying) {
+    pauseSong()
+} else if (userData?.currentSong === null) {
+  playSong(userData?.songs[0].id);
+} else {
+  playSong(userData?.currentSong.id);
+}
 });
+
+function toggleNav(e) {
+  const $navbarToggler = document.querySelector(".navbar-toggler")
+  
+  const expanded = $navbarToggler.getAttribute('aria-expanded') === 'true' || false;
+  $navbarToggler.setAttribute('aria-expanded', !expanded);
+  if (expanded) {
+    hamburgerMenu.innerHTML = `&#9776;`;
+  } else {
+    hamburgerMenu.innerHTML = `&#88;`; 
+   
+  }
+}
+navButton.addEventListener('click', toggleNav);
+
+
 progressContainer.addEventListener('click', setProgress);
 progressContainer.addEventListener('mouseup', setProgress);
 progressContainer.addEventListener('mouseup', updateProgress);
 progressContainer.addEventListener('mousedown', setProgress);
 progressContainer.addEventListener('mousedown', updateProgress);
-pauseButton.addEventListener("click",  pauseSong);
-
-nextButton.addEventListener("click", playNextSong);
-
-previousButton.addEventListener("click", playPreviousSong);
-
-shuffleButton.addEventListener("click", shuffle);
 audio.addEventListener('timeupdate', updateProgress);
 
+nextButton.addEventListener("click", playNextSong);
+nextButton.addEventListener("touch", playNextSong);
+previousButton.addEventListener("click", playPreviousSong);
+previousButton.addEventListener("touch", playPreviousSong);
 
-audio.addEventListener("ended", () => {
-  const currentSongIndex = getCurrentSongIndex();
-  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
 
-    if (nextSongExists) {
-      playNextSong();
-    } else {
-      userData.currentSong = null;
-      userData.songCurrentTime = 0;  
-pauseSong()
-highlightCurrentSong();
-setPlayButtonAccessibleText();
-    }
-});
-
-//renderSongs(userData?.songs);
-setPlayButtonAccessibleText();
